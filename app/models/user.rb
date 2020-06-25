bbc9bd32e698d5e69b630520ab64cc166b8342f7
OdongAlican <sandieo.2020@gmail.com>
2020-06-25 12:24:01 +0300

    updates user data base column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-    validates :username, presence: true, length: { minimum: 3, maximum: 25 },uniqueness: true
+    validates :name, presence: true, length: { minimum: 3, maximum: 25 },uniqueness: true
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_24_144738) do
+ActiveRecord::Schema.define(version: 2020_06_25_090437) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
