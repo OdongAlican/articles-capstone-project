b924cf76eafc3f5354174e9485fb2a4ad6022c94
OdongAlican <sandieo.2020@gmail.com>
2020-06-26 06:51:57 +0300

    adds category controller methods and routes

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   validates :name, presence: true
   validates :priority, presence: true
+  validates_uniqueness_of :priority
+  validates_uniqueness_of :name
 
   has_many :article_categories, foreign_key: :category_id, dependent: :destroy
   has_many :articles, through: :article_categories, source: :article
