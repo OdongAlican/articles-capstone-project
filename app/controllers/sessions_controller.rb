fed569cf5c04b0aae693a107e3e919083ed85d65
OdongAlican <sandieo.2020@gmail.com>
2020-06-28 02:47:39 +0300

    Adds the following - updates articles and sessions controller - updates the vote model - adds option to display the most voted article on category index - adds votes count column on articles table

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
       if user
         session[:user_id] = user.id
         flash[:success] = 'Invalid name/password combination'
-        redirect_to user_path(user)
+        redirect_to categories_path
+        # redirect_to user_path(user)
       else
         flash.now[:danger] = 'Invalid name/password combination'
         render 'new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_27_120343) do
+ActiveRecord::Schema.define(version: 2020_06_27_211104) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_06_27_120343) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
+    t.integer "votes_count"
   end
 
   create_table "categories", force: :cascade do |t|
