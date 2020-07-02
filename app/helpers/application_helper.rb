5dbc58f623ad6edbe9e115254dc308d7e2f6118b
OdongAlican <sandieo.2020@gmail.com>
2020-07-03 01:21:41 +0300

    adds logic in show, edit, new and form files in both articles and categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,40 @@ module ApplicationHelper
       link_to('Vote!', article_votes_path(article_id: article.id), method: :article, class: 'btn btn-primary')
     end
   end
+
+  def update_article
+    if current_user != nil
+        render :partial => 'rights'
+    end
+  end
+
+  def update_category
+    if current_user != nil
+      render :partial => 'authority'
+    end
+  end
+
+  def category_status(category)
+    if category.articles.length > 0
+      render :partial => 'catlength'
+    else
+      render :partial => 'emtycat'
+    end
+  end
+
+  def permit_update(article)
+    if logged_in? && current_user.id == article.author.id 
+        render :partial => 'form'
+    else
+        render :partial => 'info'
+    end
+  end
+
+  def permit_category(category)
+    if logged_in? && current_user.id == @category.user_id 
+        render :partial =>  'form'
+    else
+        render :partial =>  '/articles/info'
+    end
+  end
 end
