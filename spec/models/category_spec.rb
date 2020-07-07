1c0ce0cd05907007cb43430c2fda4ff0b6cb6b2d
OdongAlican <sandieo.2020@gmail.com>
2020-07-07 20:14:28 +0300

    updates model test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -15,13 +15,14 @@ RSpec.describe Category, type: :model do
   end
 
   describe 'orders articles order' do
-      user =  User.create!(name: 'pamla', email: 'pamla@example.com') 
-      article2 = FactoryBot.create(:article, title: 'Sundayq', text: 'sundayw@example.com', author_id: user.id)
-      article1 = FactoryBot.create(:article, title: 'johny', text: 'johndq@example.com', author_id: user.id)
-      category = Category.create!(name: 'Test', priority: 3, user_id: user.id)
-      
     it 'orders articles in decending order' do
-      expect(category.latest_article).to_not eq([article2, article1])
-    end
+      user =  User.create!(name: 'pamla', email: 'pamla@example.com') 
+      article2 = Article.create!(title: 'Newbashasnjas', text: 'ashshnasnjads@example.com', author_id: user.id)
+      article1 = Article.create!( title: 'dnaknkaakj', text: 'jsjhbasbhahs@example.com', author_id: user.id)
+      category = Category.create!(name: 'Test', priority: 3, user_id: user.id)
+        ArticleCategory.create!(article: article1, category: category)
+        ArticleCategory.create!(article: article2, category: category)
+        expect(category.latest_article).not_to eql([article2, article1])
+      end
   end
 end
