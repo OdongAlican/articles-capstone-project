4642fc7c4b8391bc4b60f250bda7b8e0f5fd0208
OdongAlican <sandieo.2020@gmail.com>
2020-07-10 14:40:35 +0300

    signs in a user after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,13 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit; end
-
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to login_path, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to categories_path, notice: "Welcome#{@user.name}" }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
