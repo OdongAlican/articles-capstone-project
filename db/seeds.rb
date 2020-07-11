9166eed14d00731deed39981ae72455a62920b16
OdongAlican <sandieo.2020@gmail.com>
2020-07-11 08:21:43 +0300

    adds category seed to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.destroy_all
+
+Category.create([
+    {   user_id: 1,
+        name: "First Category",
+        priority: 1
+    },    
+    {
+        user_id: 1,
+        name: "Second Category",
+        priority: 2
+    }
+])
+
+p "Created #{Category.count} entries"
